51633fb8235a570d7ebc7773c7dfce1a6b420207
Şiyar Vurucu <siyar.r.vurucu@gmail.com>
2019-02-14 17:26:58 +0300

    Update myga.m

--- myga.m
+++ myga.m
@@ -1,28 +1,28 @@
 tic
-Crowdness=30;                                                               % Crowdedness*
+Crowdedness=30;                                                               % Crowdedness*
 crossover_rate=1;
-crossover_fraction=(22/Crowdness); % Choose where to crossover genes. numerator should be integer and less than crowdness.
+crossover_fraction=(22/Crowdedness); % Choose where to crossover genes. numerator should be integer and less than crowdedness.
 mutation_rate=1;
 tmsize = 5;
 popsize = 150;  
 maxgen = 200;
-%preferenceMatrix = simulator(Crowdness,Popularity) % preference matrix can be generated according to any given 1x4 popularity matrix
+%preferenceMatrix = simulator(Crowdedness,Popularity) % preference matrix can be generated according to any given 1x4 popularity matrix
 bestfitness=zeros(1,maxgen);                  
 bestfitnessaverage=zeros(1,maxgen);            % initializations of best fitness matrix for the plots                 
 load('preferenceMatrices.mat');
 for k=1:10
-preferenceMatrix=preferenceMatrices(((k-1)*Crowdness+1):(k*Crowdness),:);
-Population = god(preferenceMatrix,Crowdness,popsize); % creation of initial population
-FitnessMatrix = siyar(Crowdness,preferenceMatrix,Population); %To observe fitnesses of individual population
+preferenceMatrix=preferenceMatrices(((k-1)*Crowdedness+1):(k*Crowdedness),:);
+Population = god(preferenceMatrix,Crowdedness,popsize); % creation of initial population
+FitnessMatrix = siyar(Crowdedness,preferenceMatrix,Population); %To observe fitnesses of individual population
 for j=1:maxgen
 parents = tournamentselection(Population,tmsize,popsize,FitnessMatrix);
-Offsprings=crossover(popsize,Crowdness,parents,crossover_rate,Population,crossover_fraction);
-Offsprings=mutation(Crowdness,mutation_rate,Offsprings);
+Offsprings=crossover(popsize,Crowdedness,parents,crossover_rate,Population,crossover_fraction);
+Offsprings=mutation(Crowdedness,mutation_rate,Offsprings);
 Population =[Population Offsprings];
-[FitnessMatrix, popsizenow] = siyar(Crowdness,preferenceMatrix,Population);  %popsizenow can be used to observe how population is changing. When c_rate=1, popsizenow=2*popsize
+[FitnessMatrix, popsizenow] = siyar(Crowdedness,preferenceMatrix,Population);  %popsizenow can be used to observe how population is changing. When c_rate=1, popsizenow=2*popsize
 %popsizenow
-Population=roulettewheel(Population,FitnessMatrix,Crowdness,popsize);
-FitnessMatrix = siyar(Crowdness,preferenceMatrix,Population);
+Population=roulettewheel(Population,FitnessMatrix,Crowdedness,popsize);
+FitnessMatrix = siyar(Crowdedness,preferenceMatrix,Population);
 % Info of the generation ; %
 %disp('generation number, max fitness, cost');
 j;
@@ -32,13 +32,13 @@ bestcost=60-max(FitnessMatrix);
 end
 % Info of the best solution for each preference matrix %
 indexbest=find(FitnessMatrix==max(FitnessMatrix));
-bestIndividual= Population(((indexbest(1)-1)*Crowdness+1):Crowdness*indexbest(1));
-bestIndividualfenotip=zeros(Crowdness,4);
-for i=1:Crowdness
+bestIndividual= Population(((indexbest(1)-1)*Crowdedness+1):Crowdedness*indexbest(1));
+bestIndividualfenotip=zeros(Crowdedness,4);
+for i=1:Crowdedness
     bestIndividualfenotip(i,bestIndividual(i))=1;
 end
 sprintf('Results for preference matrix %d',k)
-FitnessMatrix= siyar(Crowdness,preferenceMatrix,Population);
+FitnessMatrix= siyar(Crowdedness,preferenceMatrix,Population);
 preferenceMatrix
 bestIndividualfenotip
 courseregistered =[sum(bestIndividual==1) sum(bestIndividual==2) sum(bestIndividual==3) sum(bestIndividual==4)]
